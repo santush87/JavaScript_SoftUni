712d8a77ae87c155bd6f95c4a71b325a538b7910
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-07-22 10:08:22 +0200

    Construction Crew to 100%

---
+++ JS_Advanced/ObjectsAndComposition_Exercises/2-ConstructionCrew.js
@@ -0,0 +1,10 @@
+function solve(worker) {
+  if (worker.dizziness == true) {
+    worker.levelOfHydrated =
+      worker.levelOfHydrated + 0.1 * worker.weight * worker.experience;
+    worker.dizziness = false;
+  }
+  return worker;
+}
+
+solve({ weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true });
