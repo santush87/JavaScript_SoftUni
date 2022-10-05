cd8076627d043f385880bb438ac7fe9e09fb93e0
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-10-05 12:53:11 +0300

    Search in List to 100%

--- JS_Advanced/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ JS_Advanced/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,16 @@
 function search() {
-   // TODO
+  let towns = Array.from(document.querySelectorAll("#towns li"));
+  let userWord = document.getElementById("searchText").value;
+  let count = 0;
+  for (let town of towns) {
+    if (town.textContent.includes(userWord)) {
+      count++;
+      town.style.textDecoration = "underline";
+      town.style.fontWeight = "bold";
+    } else {
+      town.style.textDecoration = "none";
+      town.style.fontWeight = "normal";
+    }
+  }
+  document.getElementById("result").innerText = `${count} matches found`;
 }
