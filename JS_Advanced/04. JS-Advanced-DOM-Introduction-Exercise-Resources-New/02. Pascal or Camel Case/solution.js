cfd662bcb7727369a34e8bc11fd14664a56c8c9e
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-10-05 11:04:35 +0300

    Pascal or Camel Case to 100%

--- JS_Advanced/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ JS_Advanced/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById("text").value.toLowerCase().split(" ");
+
+  let command = document.getElementById("naming-convention").value;
+  let result = "";
+
+  switch (command) {
+    case "Camel Case":
+      result += input[0];
+      for (let i = 1; i < input.length; i++) {
+        let word = input[i].charAt(0).toUpperCase() + input[i].slice(1);
+        result += word;
+      }
+
+      break;
+    case "Pascal Case":
+      for (let i = 0; i < input.length; i++) {
+        let word = input[i].charAt(0).toUpperCase() + input[i].slice(1);
+        result += word;
+      }
+      break;
+    default:
+      result = "Error!";
+      break;
+  }
+  document.getElementById("result").textContent = result;
 }
