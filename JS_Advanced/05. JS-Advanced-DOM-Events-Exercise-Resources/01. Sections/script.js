2df1fbfcd32de8cd69709280620dfad2fa61487a
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-10-10 16:12:11 +0300

    Sections to 100%

--- JS_Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
+++ JS_Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+  let domTree = document.getElementById("content");
+
+  for (let i = 0; i < words.length; i++) {
+    const element = words[i];
+    let theDiv = document.createElement("div");
+    let thePar = document.createElement("p");
+    thePar.textContent = element;
+    thePar.style.display = "none";
+
+    theDiv.appendChild(thePar);
+    theDiv.addEventListener("click", shower);
+    domTree.appendChild(theDiv);
+  }
+
+  function shower(e) {
+    let p = e.target.children[0];
+    p.style.display = "block";
+  }
 }
