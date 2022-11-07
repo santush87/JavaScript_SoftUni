1fa0cadf0280ec94269a7401e5dc3bfde4dd6211
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-11-07 13:02:53 +0100

    01. Bus Stop to 100%

---
+++ JS_APPLICATIONS/06. Exercise Asynchronous Programming/01.Bus-Stop/app.js
@@ -0,0 +1,24 @@
+async function getInfo() {
+  const stopInfoElement = document.getElementById(`stopId`);
+  const stopId = stopInfoElement.value;
+  const stopNameElement = document.getElementById(`stopName`);
+  const busList = document.getElementById(`buses`);
+  busList.innerHTML = "";
+
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+  stopId.value = "";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    stopNameElement.textContent = data.name;
+    Object.entries(data.buses).forEach(([busNumber, timeArrive]) => {
+      const li = document.createElement(`li`);
+      li.textContent = `Bus ${busNumber} arrives in ${timeArrive} minutes`;
+      busList.appendChild(li);
+    });
+  } catch (error) {
+    stopNameElement.textContent = `Error`;
+  }
+}
