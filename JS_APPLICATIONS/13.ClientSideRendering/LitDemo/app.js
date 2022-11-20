c0a9c6b46dd4b000c19f0ef5988c02f8e4834efe
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-11-20 19:15:33 +0100

    Lit-HTML

--- JS_APPLICATIONS/13.ClientSideRendering/LitDemo/app.js
+++ JS_APPLICATIONS/13.ClientSideRendering/LitDemo/app.js
@@ -1,1 +1,16 @@
 import { html, render } from "https://unpkg.com/lit-html?module";
+
+// const name = "World";
+// const p = html`<p>Hello, ${name}!</p>`;
+// console.log(p);
+
+// function createP(name) {
+//   return html`<p>Hello, ${name}!</p>`;
+// }
+// render(createP("Martin"), document.querySelector("nav"));
+// render(createP("World"), document.querySelector("main"));
+
+const p = (name) => html`<p>Hello, ${name}!</p>`;
+
+render(p("Martin"), document.querySelector("nav"));
+render(p("World"), document.querySelector("main"));
