44c1bac6b8aefdf03b803199df8f5341bc35778a
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-12-08 21:34:47 +0100

    Pet Care

--- JS_APPLICATIONS/Routing/src/app.mjs
+++
@@ -1,8 +0,0 @@
-import page from "../node_modules/page/page.mjs"
-
-page("/index.html", "/")
-page("/", views[() => console.log("<h2>Home Page</h2>")])
-page("/catalog", views[() => console.log("<h2>Catalog</h2>")])
-page("/about", views[() => console.log("<h2>About</h2>")])
-
-page.start()
