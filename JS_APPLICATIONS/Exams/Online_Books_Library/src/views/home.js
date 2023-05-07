61c2c6c3cf75c5bfa475bf542b0a0b956a0fa7cb
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-05-07 15:30:59 +0200

    Added home page

--- JS_APPLICATIONS/Exams/Online_Books_Library/src/views/home.js
+++ JS_APPLICATIONS/Exams/Online_Books_Library/src/views/home.js
@@ -1,11 +1,36 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
 
-// TODO Replace with actual view
 const homeTemplate = () => html`
-    <h1>Home Page</h1>
-    <p>Welcome to our site!</p>
+  <section id="dashboard-page" class="dashboard">
+    <h1>Dashboard</h1>
+    <!-- Display ul: with list-items for All books (If any) -->
+    <ul class="other-books-list">
+      <li class="otherBooks">
+        <h3>A Court of Thorns and Roses</h3>
+        <p>Type: Fiction</p>
+        <p class="img"><img src="./images/book1.png" /></p>
+        <a class="button" href="#">Details</a>
+      </li>
+
+      <li class="otherBooks">
+        <h3>Outlander</h3>
+        <p>Type: Other</p>
+        <p class="img"><img src="/images/book2.png" /></p>
+        <a class="button" href="#">Details</a>
+      </li>
+
+      <li class="otherBooks">
+        <h3>To Kill a Mockingbird</h3>
+        <p>Type: Classic</p>
+        <p class="img"><img src="/images/book3.png" /></p>
+        <a class="button" href="#">Details</a>
+      </li>
+    </ul>
+    <!-- Display paragraph: If there are no books in the database -->
+    <p class="no-books">No books in database!</p>
+  </section>
 `;
 
 export function homePage(ctx) {
-    ctx.render(homeTemplate());
+  ctx.render(homeTemplate());
 }
