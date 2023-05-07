cea21d953f46cc94fda7d6cfe3b7ea18e460a096
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-05-07 11:23:59 +0200

    Some changes

--- JS_APPLICATIONS/Exams/Clear Career/src/views/details.js
+++ JS_APPLICATIONS/Exams/Clear Career/src/views/details.js
@@ -1,8 +1,9 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
+import { apply, getApplications, getUserApplication } from "../data/applications.js";
 import { deleteOffer, getById } from "../data/offers.js";
 import { getUserData } from "../util.js";
 
-const detailsTemplate = (offer, onDelete) => html`
+const detailsTemplate = (offer, onDelete, onApply) => html`
   <section id="details">
     <div id="details-wrapper">
       <img id="details-img" src=${offer.imageUrl} alt="example1" />
@@ -23,16 +24,26 @@ const detailsTemplate = (offer, onDelete) => html`
           <span>${offer.requirements}</span>
         </div>
       </div>
-      <!-- <p>Applications: <strong id="applications">${offer._id}</strong></p> -->
+      <p>
+        Applications: <strong id="applications">${offer.applications}</strong>
+      </p>
 
-      <!--Edit and Delete are only for creator-->
-      ${offer.canEdit
+      ${offer.canEdit || offer.canApply
         ? html` <div id="action-buttons">
-            <a href="/catalog/${offer._id}/edit" id="edit-btn">Edit</a>
-            <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a>
-
-            <!--Bonus - Only for logged-in users ( not authors )-->
-            <!-- <a href="" id="apply-btn">Apply</a> -->
+            ${offer.canEdit
+              ? html` <a href="/catalog/${offer._id}/edit" id="edit-btn"
+                    >Edit</a
+                  >
+                  <a
+                    @click=${onDelete}
+                    href="javascript:void(0)"
+                    id="delete-btn"
+                    >Delete</a
+                  >`
+              : null}
+            ${offer.canApply
+              ? html` <a href="" id="apply-btn">Apply</a>`
+              : null}
           </div>`
         : null}
     </div>
@@ -42,22 +53,35 @@ const detailsTemplate = (offer, onDelete) => html`
 export async function detailsPage(ctx) {
   const id = ctx.params.id;
 
-  const offer = await getById(id);
+  const requests = [getById(id), getApplications(id)];
 
   const userData = getUserData();
 
-  if (userData && userData._id == offer._ownerId) {
-    offer.canEdit = true;
+  if (userData) {
+    requests.push(getUserApplication(id, userData._id));
   }
 
-  ctx.render(detailsTemplate(offer, onDelete));
+  const [offer, applications, hasApplied] = await Promise.all(requests);
+  offer.applications = applications;
 
-  async function onDelete(){
-    const choice = confirm('Are you sure?');
+  if (userData) {
+    offer.canEdit = userData._id == offer._ownerId;
+    offer.canApply = offer.canEdit == false && hasApplied == 0;
+  }
+
+  ctx.render(detailsTemplate(offer, onDelete, onApply));
+
+  async function onDelete() {
+    const choice = confirm("Are you sure?");
 
     if (choice) {
       await deleteOffer(id);
-      ctx.page.redirect('/catalog');
+      ctx.page.redirect("/catalog");
     }
+  }
+
+  async function onApply(){
+    await apply(id);
+    ctx.page.redirect('/catalog/' + id);
   }
 }
