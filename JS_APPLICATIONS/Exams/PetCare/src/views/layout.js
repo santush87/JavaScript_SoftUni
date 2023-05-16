64277863badd50d3d27fb9c74d1b873580457237
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-05-16 13:18:05 +0200

    correction on logout

--- JS_APPLICATIONS/Exams/PetCare/src/views/layout.js
+++ JS_APPLICATIONS/Exams/PetCare/src/views/layout.js
@@ -1,9 +1,8 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
 import { getUserData } from "../util.js";
 
-const userData = getUserData();
+// const userData = getUserData();
 
-// TODO Replace with actual layout
 export const layoutTemplate = (userData, content) => html`
   <nav>
     <section class="logo">
@@ -15,7 +14,7 @@ export const layoutTemplate = (userData, content) => html`
 
       ${userData
         ? html` <li><a href="/create">Create Postcard</a></li>
-            <li><a href="javascript:void(0)">Logout</a></li>`
+            <li><a href="/logout">Logout</a></li>`
         : html`
             <li><a href="/login">Login</a></li>
             <li><a href="/register">Register</a></li>
