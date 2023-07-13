fd0d0e5596e6b46e002a4fa1423221f394f419db
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-07-13 10:31:01 +0300

    Added login function

---
+++ JS_APPLICATIONS/Exams/PetCare_v2.0/src/api/user.js
@@ -0,0 +1,13 @@
+import { post } from "./api.js";
+import { setUserData } from "./util.js";
+
+export async function login(email, password) {
+    const {_id, email: resultEmail, accessToken} = await post("/users/login", {email, password});
+
+    setUserData({
+        _id,
+        email: resultEmail,
+        accessToken
+    })
+}
+
