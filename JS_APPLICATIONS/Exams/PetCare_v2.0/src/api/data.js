97d6892ed71959a421df66aacc294b2bdea15476
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-07-17 19:58:24 +0300

    Added details for pets

--- JS_APPLICATIONS/Exams/PetCare_v2.0/src/api/data.js
+++ JS_APPLICATIONS/Exams/PetCare_v2.0/src/api/data.js
@@ -3,3 +3,7 @@ import { get } from "./api.js";
 export async function getAll(){
     return get("/data/pets?sortBy=_createdOn%20desc&distinct=name");
 }
+
+export async function getById(id){
+    return get("/data/pets/" + id);
+}
