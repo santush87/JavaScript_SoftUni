8285f5e0f6013cb6d04709c6a7b76ba4a5a8c569
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-07-17 20:16:15 +0300

    added hasUser and isOwner because of showing buttons

--- JS_APPLICATIONS/Exams/PetCare_v2.0/src/views/details.js
+++ JS_APPLICATIONS/Exams/PetCare_v2.0/src/views/details.js
@@ -1,7 +1,9 @@
 import { getById } from "../api/data.js";
-import { html } from "../lib.js";
+import { html, nothing } from "../lib.js";
 
-const detailsTemplate = (pet) => html` <section id="detailsPage">
+const detailsTemplate = (pet, hasUser, isOwner) => html` <section
+  id="detailsPage"
+>
   <div class="details">
     <div class="animalPic">
       <img src=${pet.image} />
@@ -14,21 +16,24 @@ const detailsTemplate = (pet) => html` <section id="detailsPage">
         <h4>Weight: ${pet.weight}</h4>
         <h4 class="donation">Donation: 0$</h4>
       </div>
-      <!-- if there is no registered user, do not display div-->
-      <div class="actionBtn">
-        <!-- Only for registered user and creator of the pets-->
-        <a href="#" class="edit">Edit</a>
-        <a href="#" class="remove">Delete</a>
-        <!--(Bonus Part) Only for no creator and user-->
-        <a href="#" class="donate">Donate</a>
-      </div>
+      ${hasUser
+        ? html` <div class="actionBtn">
+            ${isOwner
+              ? html` <a href="/edit/${pet._id}" class="edit">Edit</a>
+                  <a href="javascript:void(0)" class="remove">Delete</a>`
+              : html`<a href="#" class="donate">Donate</a>`}
+          </div>`
+        : nothing}
     </div>
   </div>
 </section>`;
 
 export async function showDetails(ctx) {
-    const id = ctx.params.id;
-    const pet = await getById(id);
+  const id = ctx.params.id;
+  const pet = await getById(id);
 
-  ctx.render(detailsTemplate(pet));
+  const hasUser = Boolean(ctx.user);
+  const isOwner = hasUser && ctx.user._id == pet._ownerId;
+
+  ctx.render(detailsTemplate(pet, hasUser, isOwner));
 }
