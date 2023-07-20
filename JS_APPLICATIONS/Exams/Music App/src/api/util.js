475af48584b5716f86ec547bf676d8732f2f9953
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-07-20 14:09:33 +0300

    Get, Set and Clear user functions added. Also createSubmitHandler

--- JS_APPLICATIONS/Exams/Music App/src/api/util.js
+++ JS_APPLICATIONS/Exams/Music App/src/api/util.js
@@ -1,11 +1,22 @@
-export function getUserData(){
-    return JSON.parse(sessionStorage.getItem("userData"));
+export function getUserData() {
+  return JSON.parse(sessionStorage.getItem("userData"));
 }
 
-export function setUserData(data){
-    sessionStorage.setItem("userData", JSON.stringify(data));
+export function setUserData(data) {
+  sessionStorage.setItem("userData", JSON.stringify(data));
 }
 
-export function clearUserData(){
-    sessionStorage.removeItem("userData");
+export function clearUserData() {
+  sessionStorage.removeItem("userData");
+}
+
+//option
+export function createSubmitHandler(callback) {
+  return function (event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    callback(data, event.target);
+  };
 }
