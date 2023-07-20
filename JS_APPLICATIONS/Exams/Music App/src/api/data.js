bac1185da4983107a283879159656c5cc509f384
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-07-20 14:10:56 +0300

    Some features needs to be done

---
+++ JS_APPLICATIONS/Exams/Music App/src/api/data.js
@@ -0,0 +1,3 @@
+import { get, post, put, del } from "./api.js";
+
+// create application service
--- JS_APPLICATIONS/Exams/Music App/src/api/user.js
+++ JS_APPLICATIONS/Exams/Music App/src/api/user.js
@@ -1,27 +1,41 @@
 import { get, post } from "./api.js";
 import { clearUserData, setUserData } from "./util.js";
 
-export async function login(email, password) {
-    const {_id, email: resultEmail, accessToken} = await post("/users/login", {email, password});
+const endpoint = {
+  login: "", //Add login endpoint!
+  register: "", //Add register endpoint!
+  logout: "", //Add logout endpoint!
+};
 
-    setUserData({
-        _id,
-        email: resultEmail,
-        accessToken
-    })
+export async function login(email, password) {
+  const {
+    _id,
+    email: resultEmail,
+    accessToken,
+  } = await post(endpoint.login, { email, password });
+
+  setUserData({
+    _id,
+    email: resultEmail,
+    accessToken,
+  });
 }
 
 export async function register(email, password) {
-    const {_id, email: resultEmail, accessToken} = await post("/users/register", {email, password});
+  const {
+    _id,
+    email: resultEmail,
+    accessToken,
+  } = await post(endpoint.register, { email, password });
 
-    setUserData({
-        _id,
-        email: resultEmail,
-        accessToken
-    })
+  setUserData({
+    _id,
+    email: resultEmail,
+    accessToken,
+  });
 }
 
-export async function logout(){
-    get("/users/logout");
-    clearUserData();
+export async function logout() {
+  get(endpoint.logout);
+  clearUserData();
 }
