dfc5210c01bc58065c61d4ca3c0537ce177129a4
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-07-20 14:08:05 +0300

    Home template added

--- JS_APPLICATIONS/Exams/Music App/src/app.js
+++ JS_APPLICATIONS/Exams/Music App/src/app.js
@@ -1,9 +1,15 @@
+import { getUserData } from "./api/util.js";
 import { page, render } from "./lib.js";
 import { showHome } from "./views/home.js";
+import { updateNav } from "./views/nav.js";
 
-const main = document.getElementById("content");
+// Get main element for renderer
+const main = document.getElementById("main-content");
 
+// Attached middle ware
 page(decorateContext);
+
+//Page routing
 page("/", showHome);
 page("/catalog", () => console.log("catalog"));
 page("/catalog/:id", () => console.log("details"));
@@ -12,11 +18,18 @@ page("/create", () => console.log("create"));
 page("/login", () => console.log("login"));
 page("/register", () => console.log("register"));
 
-
+updateNav();
 page.start();
 
 function decorateContext(ctx, next){
     ctx.render = renderMain;
+    ctx.updateNav = updateNav;
+
+    const user = getUserData();
+    if (user) {
+        ctx.user = user;
+    }
+
     next();
 }
 
--- JS_APPLICATIONS/Exams/Music App/src/views/home.js
+++ JS_APPLICATIONS/Exams/Music App/src/views/home.js
@@ -1,7 +1,18 @@
 import { html } from "../lib.js";
 
-const homeTemplate = () => html` `;
+const homeTemplate = () => html`
+  <section id="welcomePage">
+    <div id="welcome-message">
+      <h1>Welcome to</h1>
+      <h1>My Music Application!</h1>
+    </div>
 
-export function showHome(ctx){
-    ctx.render(homeTemplate());
+    <div class="music-img">
+      <img src="./images/musicIcons.webp" />
+    </div>
+  </section>
+`;
+
+export function showHome(ctx) {
+  ctx.render(homeTemplate());
 }
