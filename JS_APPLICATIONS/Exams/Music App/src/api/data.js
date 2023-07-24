5d80a0c8707f43b8a47983f4a7e167580d15336f
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-07-24 16:17:10 +0300

    added function getAllAlbums

--- JS_APPLICATIONS/Exams/Music App/src/api/data.js
+++ JS_APPLICATIONS/Exams/Music App/src/api/data.js
@@ -2,8 +2,13 @@ import { get, post, put, del } from "./api.js";
 
 const endpoints = {
   albums: "/data/albums",
+  getAllAlbums: "/data/albums?sortBy=_createdOn%20desc&distinct=name",
 };
 
 export async function createAlbum(data) {
   return post(endpoints.albums, data);
+}
+
+export async function getAllAlbums() {
+    return get(endpoints.getAllAlbums);
 }
