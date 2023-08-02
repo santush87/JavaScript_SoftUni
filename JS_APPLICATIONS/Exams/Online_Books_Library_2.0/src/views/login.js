57f415cdcf53b52bfe33df92998093942035c3f2
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-08-02 21:50:40 +0300

    Added login

--- JS_APPLICATIONS/Exams/Online_Books_Library_2.0/src/views/login.js
+++ JS_APPLICATIONS/Exams/Online_Books_Library_2.0/src/views/login.js
@@ -2,27 +2,41 @@ import { html } from "../../node_modules/lit-html/lit-html.js";
 import { login } from "../data/auth.js";
 import { createSubmitHandler } from "../util.js";
 
-// TODO Replace with actual view
-const loginTemplate = (onLogin) => html`
-    <h1>Login Page</h1>
-    <form @submit=${onLogin}>
-        <label>Email: <input type="text" name="email"></label>
-        <label>Password: <input type="password" name="password"></label>
-        <button>Login</button>
-    </form>
-`;
+const loginTemplate = (onLogin) => html`<section id="login-page" class="login">
+  <form @submit=${onLogin} id="login-form" action="" method="">
+    <fieldset>
+      <legend>Login Form</legend>
+      <p class="field">
+        <label for="email">Email</label>
+        <span class="input">
+          <input type="text" name="email" id="email" placeholder="Email" />
+        </span>
+      </p>
+      <p class="field">
+        <label for="password">Password</label>
+        <span class="input">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+        </span>
+      </p>
+      <input class="button submit" type="submit" value="Login" />
+    </fieldset>
+  </form>
+</section>`;
 
 export function loginPage(ctx) {
-    ctx.render(loginTemplate(createSubmitHandler(onLogin)));
+  ctx.render(loginTemplate(createSubmitHandler(onLogin)));
 
-    // TODO change user object based on requirements
-    async function onLogin({email, password}, form) {
-        if (email == "" || password == "") {
-            return alert("All fields are required");
-          }
-        await login(email, password);
-        form.reset();
-        // TODO use redirect location from requirements
-        ctx.page.redirect('/');
+  async function onLogin({ email, password }, form) {
+    if (email == "" || password == "") {
+      return alert("All fields are required");
     }
+    await login(email, password);
+    form.reset();
+    ctx.page.redirect("/");
+  }
 }
