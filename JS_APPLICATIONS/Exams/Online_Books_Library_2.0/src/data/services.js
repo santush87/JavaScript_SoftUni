be0940cfa2fa0fcd78d0cf9c49a053877d54fcf8
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-08-07 15:57:50 +0300

    Added 3 func for Edit page

--- JS_APPLICATIONS/Exams/Online_Books_Library_2.0/src/data/services.js
+++ JS_APPLICATIONS/Exams/Online_Books_Library_2.0/src/data/services.js
@@ -8,6 +8,10 @@ export async function createBook(book) {
   return post("/data/books", book);
 }
 
+export async function editBook(id, book) {
+  return put(`/data/books/${id}`, book);
+}
+
 export async function getMyBooks(userId) {
   return get(
     `/data/books?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`
@@ -20,4 +24,20 @@ export async function getByBookId(id) {
 
 export async function deleteByBookId(id) {
   return del("/data/books/" + id);
+}
+
+export async function likeBook(bookId) {
+  return post("/data/likes", { bookId });
+}
+
+export async function getLikesByBookId(bookId) {
+  return get(
+    `/data/likes?where=bookId%3D%22${bookId}%22&distinct=_ownerId&count`
+  );
+}
+
+export async function getMyLikeBookId(bookId, userId) {
+  return get(
+    `/data/likes?where=bookId%3D%22${bookId}%22%20and%20_ownerId%3D%22${userId}%22&count`
+  );
 }
