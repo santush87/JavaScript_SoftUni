1c1ff1f17ce9630cf548b15bc9fe31b7bd40f485
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-08-07 17:07:01 +0300

    Added register page

--- JS_APPLICATIONS/Exams/Fun Facts/src/views/register.js
+++ JS_APPLICATIONS/Exams/Fun Facts/src/views/register.js
@@ -2,22 +2,39 @@ import { html } from "../../node_modules/lit-html/lit-html.js";
 import { register } from "../data/auth.js";
 import { createSubmitHandler } from "../util.js";
 
-// TODO Replace with actual view
-const registerTemplate = (onRegister) => html`
-    <h1>Register Page</h1>
-    <form @submit=${onRegister}>
-        <label>Email: <input type="text" name="email"></label>
-        <label>Password: <input type="password" name="password"></label>
-        <label>Repeat: <input type="password" name="repass"></label>
-        <button>Register</button>
-    </form>
+const registerTemplate = (onRegister) => html`<section id="register">
+<div class="form">
+  <h2>Register</h2>
+  <form @submit=${onRegister} class="register-form">
+    <input
+      type="text"
+      name="email"
+      id="register-email"
+      placeholder="email"
+    />
+    <input
+      type="password"
+      name="password"
+      id="register-password"
+      placeholder="password"
+    />
+    <input
+      type="password"
+      name="re-password"
+      id="repeat-password"
+      placeholder="repeat password"
+    />
+    <button type="submit">register</button>
+    <p class="message">Already registered? <a href="#">Login</a></p>
+  </form>
+</div>
+</section>
 `;
 
 export function registerPage(ctx) {
     ctx.render(registerTemplate(createSubmitHandler(onRegister)));
 
-    // TODO change user object based on requirements
-    async function onRegister({email, password, repass}, form) {
+    async function onRegister({email, password, "re-password": repass}, form) {
         if (email == "" || password == "") {
             return alert("All fields are required");
         }
@@ -27,7 +44,6 @@ export function registerPage(ctx) {
 
         await register(email, password);
         form.reset();
-        // TODO use redirect location from requirements
         ctx.page.redirect('/');
     }
 }
