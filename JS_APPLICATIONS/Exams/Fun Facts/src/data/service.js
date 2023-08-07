1613d260a8920e9d47519935c87eb5fe1e81bc71
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-08-07 17:36:52 +0300

    Added create

--- JS_APPLICATIONS/Exams/Fun Facts/src/data/service.js
+++ JS_APPLICATIONS/Exams/Fun Facts/src/data/service.js
@@ -1,5 +1,9 @@
-import { get } from "./api.js";
+import { get, post } from "./api.js";
 
 export async function getAllFunFacts() {
   return get("/data/facts?sortBy=_createdOn%20desc");
+}
+
+export async function createFunFact(fact) {
+  return post("/data/facts", fact);
 }
