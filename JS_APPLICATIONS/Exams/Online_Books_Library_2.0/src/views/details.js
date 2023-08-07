0aa26c13da469610633f7bfd111ce986aa0356b1
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2023-08-07 15:57:12 +0300

    Bug fix in "edit button"

--- JS_APPLICATIONS/Exams/Online_Books_Library_2.0/src/views/details.js
+++ JS_APPLICATIONS/Exams/Online_Books_Library_2.0/src/views/details.js
@@ -1,18 +1,28 @@
 import { html, nothing } from "../../node_modules/lit-html/lit-html.js";
-import { deleteByBookId, getByBookId } from "../data/services.js";
+import {
+  deleteByBookId,
+  getByBookId,
+  getLikesByBookId,
+  getMyLikeBookId,
+  likeBook,
+} from "../data/services.js";
 import { getUserData } from "../util.js";
 
-const detailsTemplate = (book, isOwner, hasUser, onDelete) => html` <section
-  id="details-page"
-  class="details"
->
+const detailsTemplate = (
+  book,
+  isOwner,
+  likes,
+  showLikeButton,
+  onLike,
+  onDelete
+) => html` <section id="details-page" class="details">
   <div class="book-information">
     <h3>${book.title}</h3>
     <p class="type">Type: ${book.type}</p>
     <p class="img"><img src=${book.imageUrl} /></p>
     <div class="actions">
       ${isOwner
-        ? html`<a class="button" href="/edit">Edit</a>
+        ? html`<a class="button" href="/edit/${book._id}">Edit</a>
             <a @click=${onDelete} class="button" href="javascript:void(0)"
               >Delete</a
             >`
@@ -20,13 +30,14 @@ const detailsTemplate = (book, isOwner, hasUser, onDelete) => html` <section
 
       <!-- Bonus -->
       <!-- Like button ( Only for logged-in users, which is not creators of the current book ) -->
-      ${hasUser && !isOwner ? html`<a class="button" href="#">Like</a>` : nothing}
-      
+      ${showLikeButton
+        ? html`<a @click=${onLike} class="button" href="#">Like</a>`
+        : nothing}
 
       <!-- ( for Guests and Users )  -->
       <div class="likes">
         <img class="hearts" src="/images/heart.png" />
-        <span id="total-likes">Likes: 0</span>
+        <span id="total-likes">Likes: ${likes}</span>
       </div>
       <!-- Bonus -->
     </div>
@@ -42,17 +53,31 @@ export async function detailsPage(ctx) {
   const book = await getByBookId(bookId);
   const hasUser = getUserData();
   const userId = getUserData()?._id;
-
+  const likes = await getLikesByBookId(bookId);
+  const myLikes = await getMyLikeBookId(bookId, userId);
   const isOwner = book._ownerId === userId;
 
-  ctx.render(detailsTemplate(book, isOwner, hasUser, onDelete));
+  const showLikeButton = !isOwner && myLikes == 0 && userId;
+
+  ctx.render(
+    detailsTemplate(book, isOwner, likes, showLikeButton, onLike, onDelete)
+  );
 
   async function onDelete() {
-    const choice = confirm("Are you sure you want to delete this book?");
+    const choice = confirm(
+      `Are you sure you want to delete \"${book.title}\"?`
+    );
 
     if (choice) {
       await deleteByBookId(bookId);
       ctx.page.redirect("/");
     }
+  }
+
+  async function onLike() {
+    await likeBook(bookId);
+    likes++;
+    myLikes++;
+    ctx.page.redirect("/details/" + bookId);
   }
 }
